65f654dd3aec427c170c6dbb8723840ded42258e
chaerim <qkrdbsk28@naver.com>
2022-11-11 20:14:45 +0900

    feat(db): database setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return {
+          type: 'mysql',
+          host: process.env.MYSQLDB_HOST,
+          port: parseInt(process.env.MYSQLDB_DOCKER_PORT, 10),
+          username: process.env.MYSQLDB_USER,
+          password: process.env.MYSQLDB_PASSWORD,
+          database: process.env.MYSQLDB_DATABASE,
+          entities: [],
+          synchronize: true, // Fix me : set this value to false when deploy
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
