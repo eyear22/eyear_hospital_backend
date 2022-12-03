52885375969271cc1a14b137454077817dcb0246
chaerim <qkrdbsk28@naver.com>
2022-12-03 18:27:12 +0900

    fix(login): fix response data

--- src/auth/dto/login-hospital.dto.ts
+++ src/auth/dto/login-hospital.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginHospitalDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: '병원 아이디', example: 'test_id' })
   hospitalId: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: '비밀번호', example: 'test_password' })
   password!: string;
 }
