ede3a443a4aea98b249cae271100fd28e3b15f1b
chaerim <qkrdbsk28@naver.com>
2022-12-18 16:25:40 +0900

    test: injection

--- src/post/post.controller.spec.ts
+++ src/post/post.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Hospital } from '../hospital/entities/hospital.entity';
+import { Patient } from '../hospital/entities/patient.entity';
+import { User } from '../user/entities/user.entity';
 import { PostController } from './post.controller';
+import { PostService } from './post.service';
+import { Post } from './entities/post.entity';
 
 describe('PostController', () => {
   let controller: PostController;
@@ -7,6 +13,25 @@ describe('PostController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
+      providers: [
+        PostService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Hospital),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Patient),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<PostController>(PostController);
--- src/post/post.service.spec.ts
+++ src/post/post.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Hospital } from '../hospital/entities/hospital.entity';
+import { Patient } from '../hospital/entities/patient.entity';
+import { User } from '../user/entities/user.entity';
+import { Post } from './entities/post.entity';
 import { PostService } from './post.service';
+
+const mockRepository = () => ({});
 
 describe('PostService', () => {
   let service: PostService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService],
+      providers: [
+        PostService,
+        {
+          provide: getRepositoryToken(Post),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Patient),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Hospital),
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<PostService>(PostService);
