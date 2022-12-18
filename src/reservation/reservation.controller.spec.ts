7eb72ed583e1b93d29406f229eccf0c48941dbb4
chaerim <qkrdbsk28@naver.com>
2022-12-18 16:26:01 +0900

    test(reservation): changeState test

--- src/reservation/reservation.controller.spec.ts
+++ src/reservation/reservation.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Hospital } from '../hospital/entities/hospital.entity';
+import { Reservation } from './entities/reservation.entity';
 import { ReservationController } from './reservation.controller';
+import { ReservationService } from './reservation.service';
 
 describe('ReservationController', () => {
   let controller: ReservationController;
@@ -7,6 +11,17 @@ describe('ReservationController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReservationController],
+      providers: [
+        ReservationService,
+        {
+          provide: getRepositoryToken(Reservation),
+          useValue: {},
+        },
+        {
+          provide: getRepositoryToken(Hospital),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<ReservationController>(ReservationController);
