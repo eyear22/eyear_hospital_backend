c2a8afe4e41cd4aa5b5903b9c2ff3cf049d71bb0
chaerim <qkrdbsk28@naver.com>
2022-12-23 01:15:47 +0900

    refactor: post detail api

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,8 +1,5 @@
 import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Hospital } from '../hospital/entities/hospital.entity';
-import { Patient } from '../patient/entities/patient.entity';
-import { User } from '../user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { Post } from './entities/post.entity';
 
@@ -11,31 +8,16 @@ export class PostService {
   constructor(
     @InjectRepository(Post)
     private postRepository: Repository<Post>,
-
-    @InjectRepository(Hospital)
-    private hospitalRepository: Repository<Hospital>,
-
-    @InjectRepository(Patient)
-    private patientRepository: Repository<Patient>,
-
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
   ) {
     this.postRepository = postRepository;
-    this.hospitalRepository = hospitalRepository;
-    this.patientRepository = patientRepository;
-    this.userRepository = userRepository;
   }
 
   async getPostDetail(postId: number, hospitalId: string) {
-    const post = await this.postRepository
-      .createQueryBuilder('post')
-      .leftJoin('post.hospital', 'hospital')
-      .where('post.id =:postId', { postId })
-      .andWhere('hospital.hospitalId =:hospitalId', { hospitalId })
-      .execute();
+    const post = await this.postRepository.findOne({
+      where: { id: postId, hospital: { hospitalId: hospitalId } },
+    });
 
-    if (post.length != 1) {
+    if (post === null) {
       throw new NotFoundException({
         statusCode: HttpStatus.NOT_FOUND,
         message: ['not existed post'],
@@ -44,13 +26,20 @@ export class PostService {
     }
 
     return {
-      id: post[0].post_id,
-      video: post[0].post_video,
-      text: post[0].post_text,
-      check: post[0].post_check,
-      stampNumber: post[0].post_stampNumber,
-      cardNumber: post[0].post_cardNumber,
-      createdAt: post[0].post_createdAt.toISOString().split('T')[0],
+      id: post.id,
+      video: post.video,
+      text: post.text,
+      check: post.check,
+      stampNumber: post.stampNumber,
+      cardNumber: post.cardNumber,
+      createdAt: this.formatDate(post.createdAt),
     };
+  }
+
+  formatDate(data: Date): string {
+    const temp = data.toISOString().split('T')[0];
+    const temp2 = temp.split('-');
+
+    return temp2[0].substring(2) + '/' + temp2[1] + '/' + temp2[2];
   }
 }
